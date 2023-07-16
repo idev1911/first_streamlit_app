beb685730cb4b17c0b0937dfe4af82a440440fba
idev1911 <139607747+idev1911@users.noreply.github.com>
2023-07-16 00:17:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ except URLError as e:
 #streamlit.write('The user entered ', fruit_choice)
 
 
-streamlit.stop()
+#streamlit.stop()
 
 def get_fruit_load_list():
    with my_cnx.cursor() as my_cur:
